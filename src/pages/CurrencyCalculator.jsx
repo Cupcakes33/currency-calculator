c7e5971b27575ac53f682c730b0bd3b4b9433548
Cupcakes33 <rute930627@gmail.com>
2023-02-27 22:07:19 +0900

    :sparkles: feat: 날짜 바인딩, 반올림 로직 함수화

--- src/pages/CurrencyCalculator.jsx
+++ src/pages/CurrencyCalculator.jsx
@@ -3,14 +3,36 @@ import styled from "styled-components";
 import useExchangeData from "../hooks/useExchangeData";
 
 const CurrencyCalculator = () => {
-  const [base, setBase] = useState("EUR");
-  const [target, setTarget] = useState("GBP");
+  const [base, setBase] = useState("USD");
+  const [target, setTarget] = useState("KRW");
   const [baseCurrency, setBaseCurrency] = useState(1);
+  // const [exchangeData, error] = useExchangeData(base, target);
 
-  const [exchangeData, error] = useExchangeData(base, target);
+  const exchangeData = {
+    conversion_rate: 1000,
+    time_last_update_utc: "Mon, 27 Feb 2023 00:00:01 +0000",
+  };
+
+  const locailDate = (date) => {
+    return new Date(date).toLocaleString();
+  };
+
+  console.log(locailDate(exchangeData?.time_last_update_utc));
+
+  const roundExchangeData = (base, exchange) => {
+    return Math.round(base * exchange * 1000) / 1000;
+  };
 
   return (
     <Container>
+      <p>{`${baseCurrency} ${base} = `}</p>
+      <h3>
+        {`${roundExchangeData(
+          baseCurrency,
+          exchangeData?.conversion_rate
+        )} ${target}`}
+      </h3>
+      <p>{locailDate(exchangeData?.time_last_update_utc)}</p>
       <img src={`https://flagsapi.com/BE/shiny/64.png`} alt="base flag" />
       <img src={`https://flagsapi.com/BE/shiny/64.png`} alt="target flag" />
 
@@ -21,15 +43,14 @@ const CurrencyCalculator = () => {
         defaultValue={baseCurrency}
         onChange={(e) => setBaseCurrency(e.target.value)}
       />
-      {exchangeData ? (
-        <span>
-          {baseCurrency}
-          {base} =
-          {Math.round(baseCurrency * exchangeData.conversion_rate * 1000) /
-            1000}
-          {target}
-        </span>
-      ) : null}
+      {/* {exchangeData ? ( */}
+      <span>
+        {baseCurrency}
+        {base} =
+        {Math.round(baseCurrency * exchangeData.conversion_rate * 1000) / 1000}
+        {target}
+      </span>
+      {/* ) : null} */}
     </Container>
   );
 };
@@ -37,6 +58,14 @@ const CurrencyCalculator = () => {
 export default CurrencyCalculator;
 
 const Container = styled.div`
+  width: 300px;
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 16px;
+  background: #efefef;
   img {
     width: 30px;
   }
