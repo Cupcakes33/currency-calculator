1b48d99cab1b8b194789b6e415eade49cc5e446d
Cupcakes33 <rute930627@gmail.com>
2023-03-01 10:16:38 +0900

    :refector: fix: 변수명 수정

--- src/pages/CurrencyCalculator.jsx
+++ src/pages/CurrencyCalculator.jsx
@@ -5,19 +5,19 @@ import CurrencyInput from "../components/CurrencyInput";
 import { exchangeAPI } from "../apis/axios";
 
 const CurrencyCalculator = () => {
-  const [base, setBase] = useState("미국 달러");
-  const [target, setTarget] = useState("한국 원");
+  const [baseCurrency, setBaseCurrency] = useState("미국 달러");
+  const [targetCurrency, setTargetCurrency] = useState("한국 원");
   const [baseValue, setBaseValue] = useState(1);
   const [targetValue, setTargetValue] = useState(1);
   const [conversionRate, setConversionRate] = useState({});
-  const [timeLastUpdate, setTimeLastUpdate] = useState("");
+  const [timeLastUpdated, setTimeLastUpdated] = useState("");
 
   useEffect(() => {
-    exchangeAPI.get(COUNTRY_LIST[base]).then((res) => {
-      setTimeLastUpdate(res.time_last_update_utc);
+    exchangeAPI.get(COUNTRY_LIST[baseCurrency]).then((res) => {
+      setTimeLastUpdated(res.time_last_update_utc);
       setConversionRate(res.conversion_rates);
     });
-  }, [base]);
+  }, [baseCurrency]);
 
   const roundExchangeData = (exchange) => {
     return Math.round(exchange * 1000) / 1000;
@@ -25,74 +25,73 @@ const CurrencyCalculator = () => {
 
   const locailDate = useCallback(() => {
     const options = {
-      timeZone: "UTC",
       month: "long",
       day: "numeric",
       hour: "numeric",
       minute: "numeric",
     };
-    return `${new Date(timeLastUpdate).toLocaleString("ko-KR", options)} UTC`;
-  }, [timeLastUpdate]);
+    return `${new Date(timeLastUpdated).toLocaleString("ko-KR", options)} UTC`;
+  }, [timeLastUpdated]);
 
   const baseValueChangeHandler = (baseValue) => {
     setTargetValue(
       roundExchangeData(
-        (baseValue * conversionRate[COUNTRY_LIST[target]]) /
-          conversionRate[COUNTRY_LIST[base]]
+        (baseValue * conversionRate[COUNTRY_LIST[targetCurrency]]) /
+          conversionRate[COUNTRY_LIST[baseCurrency]]
       )
     );
     setBaseValue(baseValue);
   };
 
-  const baseCountryChangeHandler = (base) => {
+  const baseCountryChangeHandler = (baseCurrency) => {
     setTargetValue(
       roundExchangeData(
-        (baseValue * conversionRate[COUNTRY_LIST[target]]) /
-          conversionRate[COUNTRY_LIST[base]]
+        (baseValue * conversionRate[COUNTRY_LIST[targetCurrency]]) /
+          conversionRate[COUNTRY_LIST[baseCurrency]]
       )
     );
-    setBase(base);
+    setBaseCurrency(baseCurrency);
   };
 
   const targetValueChangeHandler = (targetValue) => {
     setBaseValue(
       roundExchangeData(
-        (targetValue * conversionRate[COUNTRY_LIST[base]]) /
-          conversionRate[COUNTRY_LIST[target]]
+        (targetValue * conversionRate[COUNTRY_LIST[baseCurrency]]) /
+          conversionRate[COUNTRY_LIST[targetCurrency]]
       )
     );
     setTargetValue(targetValue);
   };
 
-  const targetCountryChangeHandler = (target) => {
+  const targetCountryChangeHandler = (targetCurrency) => {
     setBaseValue(
       roundExchangeData(
-        (targetValue * conversionRate[COUNTRY_LIST[base]]) /
-          conversionRate[COUNTRY_LIST[target]]
+        (targetValue * conversionRate[COUNTRY_LIST[baseCurrency]]) /
+          conversionRate[COUNTRY_LIST[targetCurrency]]
       )
     );
-    setTarget(target);
+    setTargetCurrency(targetCurrency);
   };
 
   return (
     <Container>
       <BaseExchangeRateWrapper>
-        <p>{`1 ${base} = `}</p>
+        <p>{`1 ${baseCurrency} = `}</p>
         <h1>{`${roundExchangeData(
-          conversionRate[COUNTRY_LIST[target]]
-        )} ${target}`}</h1>
+          conversionRate[COUNTRY_LIST[targetCurrency]]
+        )} ${targetCurrency}`}</h1>
         <p>{locailDate()}</p>
       </BaseExchangeRateWrapper>
 
       <CalculatorWrapper>
         <CurrencyInput
-          country={base}
+          country={baseCurrency}
           setCountry={baseCountryChangeHandler}
           value={baseValue}
           setValue={baseValueChangeHandler}
         />
         <CurrencyInput
-          country={target}
+          country={targetCurrency}
           setCountry={targetCountryChangeHandler}
           value={targetValue}
           setValue={targetValueChangeHandler}
